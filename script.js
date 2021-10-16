ecf56a1e1cad3698d423778303f36159c152f756
amakaogujiofor <80483692+amakaogujiofor@users.noreply.github.com>
2021-10-16 13:36:44 +0100

    Modified code

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myEmojis = ["👨‍💻", "⛷", "🍲"];
 const container = document.getElementById("emojiContainer");
 const button = document.getElementById("push-btn");
 const input = document.getElementById("emoji-input");
+const unshift = document.getElementById("front-btn");
 
 // for (let i = 0; i < myEmojis.length; i++) {
 //   const emoji = document.createElement("span");
@@ -17,9 +18,20 @@ function render() {
 
 render();
 
+// Push Button
 button.addEventListener("click", function () {
   if (input.value) {
     myEmojis.push(input.value);
+    input.value = "";
+    container.innerHTML = "";
+    render();
+  }
+});
+
+// Unshift Button
+unshift.addEventListener("click", function () {
+  if (input.value) {
+    myEmojis.unshift(input.value);
     input.value = "";
     container.innerHTML = "";
     render();
